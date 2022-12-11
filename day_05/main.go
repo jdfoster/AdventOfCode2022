be5ba6e1a4d676237912878931f127db348e8118
Joshua Foster <joshua.david.foster@gmail.com>
2022-12-11 13:26:41 +0000

    feat: day 5 part a

---
+++ day_05/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"unicode"
+)
+
+var fn = "./day_05/input.txt"
+
+type Stack struct {
+	stack []rune
+}
+
+func (s *Stack) Shift(v rune) {
+	s.stack = append([]rune{v}, s.stack...)
+}
+
+func (s *Stack) Unshift() rune {
+	r := s.stack[0]
+	s.stack = s.stack[1:]
+
+	return r
+}
+
+func (s *Stack) Push(v rune) {
+	s.stack = append(s.stack, v)
+}
+
+func (s Stack) Head() rune {
+	if len(s.stack) == 0 {
+		return '-'
+	}
+
+	return s.stack[0]
+}
+
+type Grid struct {
+	stacks []*Stack
+}
+
+func (g Grid) Head() string {
+	res := make([]rune, len(g.stacks))
+
+	for i, v := range g.stacks {
+		res[i] = v.Head()
+	}
+
+	return string(res)
+}
+
+func (g Grid) Insert(i int, v rune) {
+	s := g.stacks[i]
+	s.Push(v)
+}
+
+func (g Grid) Move(c, x, y int) {
+	a := g.stacks[x-1]
+	b := g.stacks[y-1]
+
+	for i := 0; i < c; i++ {
+		v := a.Unshift()
+		b.Shift(v)
+	}
+}
+
+func NewGrid() *Grid {
+	stacks := make([]*Stack, 9)
+
+	for i := range stacks {
+		stacks[i] = &Stack{}
+	}
+
+	return &Grid{
+		stacks: stacks,
+	}
+}
+
+func parseGridLine(g *Grid, l string) {
+	rr := []rune(l)
+
+	for i, r := range rr {
+		if unicode.IsLetter(r) {
+			g.Insert((i-1)/4, r)
+		}
+	}
+}
+
+var re = regexp.MustCompile(`\d+`)
+
+func parseMoveLine(g *Grid, l string) {
+	r := re.FindAll([]byte(l), -1)
+	if len(r) != 3 {
+		panic("unexpected count")
+	}
+
+	n := make([]int, 3)
+	for i, v := range r {
+		j, err := strconv.ParseInt(string(v), 10, 0)
+		if err != nil {
+			panic("failed to parse number")
+		}
+		n[i] = int(j)
+	}
+
+	g.Move(n[0], n[1], n[2])
+}
+
+func main() {
+	f, err := os.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	grid := NewGrid()
+
+	for s.Scan() {
+		line := s.Text()
+
+		switch len(line) {
+		case 35:
+			parseGridLine(grid, line)
+		case 0, 1:
+			// break
+		default:
+			parseMoveLine(grid, line)
+
+		}
+
+	}
+
+	fmt.Println(grid.Head())
+}
