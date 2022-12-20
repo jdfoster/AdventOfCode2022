977e75344f4e39633f363bcff5d60064fcf5bf83
Joshua Foster <joshua.david.foster@gmail.com>
2022-12-20 11:14:39 +0000

    feat: day 9 part b

--- day_09/main_test.go
+++ day_09/main_test.go
@@ -48,104 +48,123 @@ func TestMain(t *testing.T) {
 		})
 	})
 
-	t.Run("Ending", func(t *testing.T) {
+	t.Run("Knot", func(t *testing.T) {
 		t.Run("should record seen positions", func(t *testing.T) {
-			ending := NewEnding()
+			knot := NewKnot()
 
-			if got, want := ending.Count(), 1; got != want {
+			if got, want := knot.Count(), 1; got != want {
 				t.Errorf("got %d, want %d", got, want)
 			}
 
-			ending.Move(1, 0)
-			ending.Move(1, 0)
-			ending.Move(0, 1)
-			ending.Move(0, 1)
+			knot.Move(1, 0)
+			knot.Move(1, 0)
+			knot.Move(0, 1)
+			knot.Move(0, 1)
 
-			if got, want := ending.Count(), 5; got != want {
+			if got, want := knot.Count(), 5; got != want {
 				t.Errorf("got %d, want %d", got, want)
 			}
 
-			testPosition(t, ending.pos, Position{X: 2, Y: 2})
+			testPosition(t, knot.pos, Position{X: 2, Y: 2})
 
-			ending.Move(0, -1) // already visited
+			knot.Move(0, -1) // already visited
 
-			if got, want := ending.Count(), 5; got != want {
+			if got, want := knot.Count(), 5; got != want {
 				t.Errorf("got %d, want %d", got, want)
 			}
 
-			testPosition(t, ending.pos, Position{X: 2, Y: 1})
+			testPosition(t, knot.pos, Position{X: 2, Y: 1})
 		})
 	})
 
 	t.Run("Rope", func(t *testing.T) {
 		t.Run("should move tail to follow head", func(t *testing.T) {
-			rope := NewRope()
+			rope := NewRope(2)
 
 			// move head right 1; tail should not move
 			rope.Move(1, 0)
-			testPosition(t, rope.Head.pos, Position{X: 1, Y: 0})
-			testPosition(t, rope.Tail.pos, Position{X: 0, Y: 0})
+			testPosition(t, rope.knots[0].pos, Position{X: 1, Y: 0})
+			testPosition(t, rope.knots[1].pos, Position{X: 0, Y: 0})
 
 			// move head right 1; tail should follow
 			rope.Move(1, 0)
-			testPosition(t, rope.Head.pos, Position{X: 2, Y: 0})
-			testPosition(t, rope.Tail.pos, Position{X: 1, Y: 0})
+			testPosition(t, rope.knots[0].pos, Position{X: 2, Y: 0})
+			testPosition(t, rope.knots[1].pos, Position{X: 1, Y: 0})
 
 			// move head right 1; tail should follow
 			rope.Move(1, 0)
-			testPosition(t, rope.Head.pos, Position{X: 3, Y: 0})
-			testPosition(t, rope.Tail.pos, Position{X: 2, Y: 0})
+			testPosition(t, rope.knots[0].pos, Position{X: 3, Y: 0})
+			testPosition(t, rope.knots[1].pos, Position{X: 2, Y: 0})
 
 			// move head up 1; tail should not move
 			rope.Move(0, 1)
-			testPosition(t, rope.Head.pos, Position{X: 3, Y: 1})
-			testPosition(t, rope.Tail.pos, Position{X: 2, Y: 0})
+			testPosition(t, rope.knots[0].pos, Position{X: 3, Y: 1})
+			testPosition(t, rope.knots[1].pos, Position{X: 2, Y: 0})
 
 			// move head up 1; tail should move diagonally
 			rope.Move(0, 1)
-			testPosition(t, rope.Head.pos, Position{X: 3, Y: 2})
-			testPosition(t, rope.Tail.pos, Position{X: 3, Y: 1})
+			testPosition(t, rope.knots[0].pos, Position{X: 3, Y: 2})
+			testPosition(t, rope.knots[1].pos, Position{X: 3, Y: 1})
 
 			// move head up 1; tail should follow
 			rope.Move(0, 1)
-			testPosition(t, rope.Head.pos, Position{X: 3, Y: 3})
-			testPosition(t, rope.Tail.pos, Position{X: 3, Y: 2})
+			testPosition(t, rope.knots[0].pos, Position{X: 3, Y: 3})
+			testPosition(t, rope.knots[1].pos, Position{X: 3, Y: 2})
 
 			// move head left 1; tail should not move
 			rope.Move(-1, 0)
-			testPosition(t, rope.Head.pos, Position{X: 2, Y: 3})
-			testPosition(t, rope.Tail.pos, Position{X: 3, Y: 2})
+			testPosition(t, rope.knots[0].pos, Position{X: 2, Y: 3})
+			testPosition(t, rope.knots[1].pos, Position{X: 3, Y: 2})
 
 			// move head left 1; tail should move diagonally
 			rope.Move(-1, 0)
-			testPosition(t, rope.Head.pos, Position{X: 1, Y: 3})
-			testPosition(t, rope.Tail.pos, Position{X: 2, Y: 3})
+			testPosition(t, rope.knots[0].pos, Position{X: 1, Y: 3})
+			testPosition(t, rope.knots[1].pos, Position{X: 2, Y: 3})
 
 			// move head left 1; tail should follow
 			rope.Move(-1, 0)
-			testPosition(t, rope.Head.pos, Position{X: 0, Y: 3})
-			testPosition(t, rope.Tail.pos, Position{X: 1, Y: 3})
+			testPosition(t, rope.knots[0].pos, Position{X: 0, Y: 3})
+			testPosition(t, rope.knots[1].pos, Position{X: 1, Y: 3})
 
 			// move head down 1; tail should not move
 			rope.Move(0, -1)
-			testPosition(t, rope.Head.pos, Position{X: 0, Y: 2})
-			testPosition(t, rope.Tail.pos, Position{X: 1, Y: 3})
+			testPosition(t, rope.knots[0].pos, Position{X: 0, Y: 2})
+			testPosition(t, rope.knots[1].pos, Position{X: 1, Y: 3})
 
 			// move head down 1; tail should move diagonally
 			rope.Move(0, -1)
-			testPosition(t, rope.Head.pos, Position{X: 0, Y: 1})
-			testPosition(t, rope.Tail.pos, Position{X: 0, Y: 2})
+			testPosition(t, rope.knots[0].pos, Position{X: 0, Y: 1})
+			testPosition(t, rope.knots[1].pos, Position{X: 0, Y: 2})
 		})
 	})
 
 	t.Run("Scan", func(t *testing.T) {
 		input := "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
 
-		t.Run("should parse input", func(t *testing.T) {
-			r := strings.NewReader(input)
-			rope := Scan(r)
+		t.Run("should parse input to move 2 knot rope", func(t *testing.T) {
+			rope := NewRope(2)
+			Scan(strings.NewReader(input), rope)
 
-			if got, want := rope.Tail.Count(), 13; got != want {
+			if got, want := rope.Tail().Count(), 13; got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+
+		t.Run("should parse input to move 10 knot rope", func(t *testing.T) {
+			rope := NewRope(10)
+			Scan(strings.NewReader(input), rope)
+
+			if got, want := rope.Tail().Count(), 1; got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+
+		t.Run("should parse larger input to move 10 knot rope", func(t *testing.T) {
+			long := "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20"
+			rope := NewRope(10)
+			Scan(strings.NewReader(long), rope)
+
+			if got, want := rope.Tail().Count(), 36; got != want {
 				t.Errorf("got %d, want %d", got, want)
 			}
 		})
