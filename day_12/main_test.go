a8db75cb5aaa842346aef16f703eb479f0fc3421
Joshua Foster <joshua.david.foster@gmail.com>
2022-12-24 13:27:10 +0000

    feat: day 12 part b

--- day_12/main_test.go
+++ day_12/main_test.go
@@ -39,28 +39,43 @@ func TestMain(t *testing.T) {
 	t.Run("finds next positions", func(t *testing.T) {
 		r := strings.NewReader(input)
 		grid, _, end := Scan(r)
+		next := Position{X: 4, Y: 2}
 
-		v := grid.EqualOrAbove(Position{X: 4, Y: 2})
+		v := grid.EqualOrLower(end)
 
 		if got, want := len(v), 1; got != want {
 			t.Errorf("got %d, want %d", got, want)
 		}
 
-		if !end.Equal(v[0]) {
-			t.Error("End not found")
+		if !next.Equal(v[0]) {
+			t.Error("end not found")
 		}
 	})
 
 	t.Run("calculate shortest path", func(t *testing.T) {
 		r := strings.NewReader(input)
 		grid, start, end := Scan(r)
-		res, ok := Walk(grid, start, end)
+		res, ok := Walk(grid, end, start.Equal)
 		if !ok {
 			t.Fatal("failed to find path to goal")
 		}
 
 		if got, want := res.Count, 31; got != want {
 			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("calculate closest square with elevation \"a\"", func(t *testing.T) {
+		r := strings.NewReader(input)
+		grid, _, end := Scan(r)
+		fin := grid.FindElevation('a')
+		res, ok := Walk(grid, end, fin)
+		if !ok {
+			t.Fatal("failed to find square with elevation 'a'")
+		}
+
+		if got, want := res.Count, 29; got != want {
+			t.Errorf("got %d, wamt %d", got, want)
 		}
 	})
 }
